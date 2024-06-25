782aa3b68ed9ecdd177b0a06b8f496493406cb4b
Nikola Jovanovic <jovanovicn369@gmail.com>
2024-06-25 16:58:58 +0200

    Bugfix- last flush in second transform was pushed as array, instead of going through array items, Refactor of two streams for readability

--- src/transform.ts
+++ src/transform.ts
@@ -1,31 +1,26 @@
 import { Transform } from "stream";
 import { tryParseJson } from "./helpers";
-import { DataInterface } from "./types/interfaces";
 import path from "path";
+import { TransformedData } from "./types/interfaces";
 
-export const createStringChunkToObjectTransform = () => {
+export const createStringChunkToFileUrlTransform = () => {
   const delimiter = ",";
-  let tail = "";
+  let buffer = "";
 
   return new Transform({
     defaultEncoding: "utf8",
     transform(chunk, encoding, cb) {
       try {
-        if (!chunk) {
-          cb();
-        }
-        const pieces = (tail + chunk)
+        buffer += chunk.toString();
+        const items = buffer
           .replace('{"items":[', "")
           .replace("}]", "")
           .split(delimiter);
-        const lastPiece = pieces[pieces.length - 1];
-        const tailLen = delimiter.length - 1;
-        tail = lastPiece.slice(-tailLen);
-        pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen);
+        buffer = items.pop() || "";
 
-        for (const piece of pieces) {
-          const parsedObject = tryParseJson(piece);
-          if (parsedObject && parsedObject.fileUrl) {
+        for (const item of items) {
+          const parsedObject = tryParseJson(item);
+          if (parsedObject?.fileUrl) {
             this.push(parsedObject.fileUrl);
           }
         }
@@ -35,8 +30,8 @@ export const createStringChunkToObjectTransform = () => {
       }
     },
     flush(cb) {
-      const parsedObject = tryParseJson(tail);
-      if (parsedObject && parsedObject.fileUrl) {
+      const parsedObject = tryParseJson(buffer);
+      if (parsedObject?.fileUrl) {
         this.push(parsedObject.fileUrl);
       }
       cb();
@@ -45,79 +40,81 @@ export const createStringChunkToObjectTransform = () => {
 };
 
 export const createUrlToObjectTransform = () => {
-  let transformedObject: any = {};
+  let transformedObject: TransformedData = {} as TransformedData;
 
   let currentIp = "";
   let currentDirectory = "";
-  let startOfStream = true;
-  let count = 0;
+  let isFirstChunk = true;
 
-  const addExtraData = (chunk: string): string => {
-    let chunkReadyToSend = "";
-    if (startOfStream) {
-      chunkReadyToSend += `{"${currentIp}":[${chunk}`;
-    } else {
-      chunkReadyToSend += `,${chunk}`;
+  const formatChunk = (chunk: string): string => {
+    if (isFirstChunk) {
+      isFirstChunk = false;
+      return `{"${currentIp}":[${chunk}`;
     }
-    startOfStream = false;
-    return chunkReadyToSend;
+    return `,${chunk}`;
+  };
+
+  const processUrl = (url: string, push: (chunk: any) => void): void => {
+    const urlObj = new URL(decodeURI(url));
+    const ipAddress = urlObj.hostname;
+    const pathSegments = urlObj.pathname.split("/").filter(Boolean);
+    currentIp = ipAddress;
+
+    if (currentDirectory && currentDirectory !== pathSegments[0]) {
+      flushCurrentDirectoryData(push);
+    }
+
+    currentDirectory = pathSegments[0];
+
+    transformedObject[ipAddress] = transformedObject[ipAddress] || [];
+    let currentLevel = transformedObject[ipAddress];
+
+    for (let i = 0; i < pathSegments.length; i++) {
+      const segment = pathSegments[i];
+      if (i === pathSegments.length - 1 && path.extname(segment)) {
+        currentLevel.push(segment);
+      } else {
+        let nextLevel = currentLevel.find(
+          (item): item is { [key: string]: any } =>
+            typeof item === "object" && segment in item
+        );
+        if (!nextLevel) {
+          nextLevel = { [segment]: [] };
+          currentLevel.push(nextLevel);
+        }
+        currentLevel = nextLevel[segment];
+      }
+    }
+  };
+
+  const flushCurrentDirectoryData = (push: (chunk: any) => void): void => {
+    if (currentIp && transformedObject[currentIp]) {
+      const dataToSend = transformedObject[currentIp].shift();
+      if (dataToSend) {
+        push(formatChunk(JSON.stringify(dataToSend)));
+      }
+    }
   };
 
   return new Transform({
     objectMode: true,
     transform(chunk: any, encoding: any, callback: any) {
       try {
-       
-        const urlObj = new URL(decodeURI(chunk));
-        const ipAddress = urlObj.hostname;
-        const pathSegments = urlObj.pathname
-          .split("/")
-          .filter((segment) => segment);
-
-        currentIp = ipAddress;
-
-        if (currentDirectory && currentDirectory !== pathSegments[0]) {
-
-          const chunkToSend = addExtraData(
-            JSON.stringify(transformedObject[currentIp].shift())
-          );
-          currentDirectory = pathSegments[0];
-          if (chunkToSend) {
-            this.push(chunkToSend);
-          }
-        }
-        currentDirectory = pathSegments[0];
-        if (!transformedObject[ipAddress]) {
-          transformedObject[ipAddress] = [];
-        }
-        let currentLevel = transformedObject[ipAddress];
-        for (let i = 0; i < pathSegments.length; i++) {
-          const segment = pathSegments[i];
-          const segmentLevel =  pathSegments.length - 1;
-          if (i === segmentLevel && path.extname(segment)) {
-            currentLevel.push(segment);
-          } else {
-            let nextLevel = currentLevel.find(
-              (item: any) => typeof item === "object" && item[segment]
-            );
-            if (!nextLevel) {
-              nextLevel = { [segment]: [] };
-              currentLevel.push(nextLevel);
-            }
-            currentLevel = nextLevel[segment];
-          }
-        }
+        processUrl(chunk, this.push.bind(this));
         callback();
       } catch (err) {
         callback(err);
       }
     },
-    flush (cb) {
-      let lastChunk = addExtraData(JSON.stringify(transformedObject[currentIp]));
-      lastChunk += ']}';
+    flush(cb) {
+      const lastObject = transformedObject[currentIp] || [];
+      let lastChunk = "";
+      for (const item of lastObject) {
+        lastChunk += formatChunk(JSON.stringify(item));
+      }
+      lastChunk += "]}";
       this.push(lastChunk);
       cb();
-       }
+    },
   });
 };
-
